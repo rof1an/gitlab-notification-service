3a74b5e976e22a65bc43355ffec8a9a823e912b5
rof1an <egorrachkov0@gmail.com>
2025-01-30 15:11:56 +0300

    init

---
+++ src/main/java/com/notification/service/NotificationApplication.java
@@ -0,0 +1,13 @@
+package com.notification.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
