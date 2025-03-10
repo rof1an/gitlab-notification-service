8b6fe1658d80c38abca198e123b125b20bc6a091
rof1an <egorrachkov0@gmail.com>
2025-03-10 12:13:34 +0300

    added base logic to telegram bot

---
+++ src/main/java/com/notification/service/telegram/HiveNotificationBot.java
@@ -0,0 +1,171 @@
+package com.notification.service.telegram;
+
+import com.notification.service.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class HiveNotificationBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botUsername;
+
+    private final TaskService taskService;
+
+    private final String START = "/start";
+    private final String GET_REPLY_BUTTONS = "/get_reply_buttons";
+    private final String GET_INLINE_BUTTONS = "/get_inline_buttons";
+
+    public HiveNotificationBot(@Value("${bot.token}") String botToken, TaskService taskService) {
+        super(botToken);
+        this.taskService = taskService;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText();
+            String chatId = update.getMessage().getChatId().toString();
+
+            switch (message) {
+                case START -> {
+                    String userName = update.getMessage().getChat().getFirstName();
+                    startCommand(chatId, userName);
+                }
+                case GET_REPLY_BUTTONS -> {
+                    sendReplyKeyboard(chatId);
+                }
+                case GET_INLINE_BUTTONS -> {
+                    sendInlineKeyboard(chatId);
+                }
+                case "Кнопка 1" -> {
+                    sendMessage(chatId, "Вы нажали кнопку 1");
+                }
+                case "Кнопка 2" -> {
+                    sendMessage(chatId, "Вы нажали кнопку 2");
+                }
+                case "Кнопка 3" -> {
+                    sendMessage(chatId, "Вы нажали кнопку 3");
+                }
+                default -> {
+                    defaultCommand(chatId);
+                }
+            }
+        }
+    }
+
+    private ReplyKeyboardMarkup createReplyKeyboardMarkup() {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("Кнопка 1");
+        row1.add("Кнопка 2");
+
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add("Кнопка 3");
+
+        keyboard.add(row1);
+        keyboard.add(row2);
+
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+
+        return replyKeyboardMarkup;
+    }
+
+    private void sendReplyKeyboard(String chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = createReplyKeyboardMarkup();
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Выберите действие");
+        message.setReplyMarkup(replyKeyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void sendInlineKeyboard(String chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        List<InlineKeyboardButton> row = new ArrayList<>();
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Нажми меня");
+        button1.setCallbackData("button_pressed");
+
+        row.add(button1);
+        keyboard.add(row);
+
+        inlineKeyboardMarkup.setKeyboard(keyboard);
+
+        SendMessage message = new SendMessage(chatId, "Выберите действие:");
+        message.setReplyMarkup(inlineKeyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Ошибка отправки сообщения", e);
+        }
+    }
+
+    private void startCommand(String chatId, String userName) {
+        String text = """
+                Добро пожаловать в бот, %s.
+                Здесь можно увидеть список МР.
+                                
+                Команды для использования:
+                /start - запуск бота
+                /get_reply_buttons
+                /get_inline_buttons
+                """;
+
+        String formattedText = String.format(text, userName);
+        sendMessage(chatId, formattedText);
+    }
+
+    private void defaultCommand(String chatId) {
+        String formattedText = """
+                Команды для использования:
+                /start - запуск бота
+                /get_reply_buttons
+                /get_inline_buttons
+                """;
+
+        sendMessage(chatId, formattedText);
+    }
+
+    private void sendMessage(String chatId, String text) {
+        SendMessage sendMessage = new SendMessage(chatId, text);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Ошибка отправки сообщения", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+}
