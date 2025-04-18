cde5a188b83f206a65a3d4b4ec7dd48bec813195
Yegor <91024892+rof1an@users.noreply.github.com>
2025-04-18 21:50:03 +0300

    refactored NotificationService (#25)

--- src/main/java/com/notification/service/service/NotificationService.java
+++ src/main/java/com/notification/service/service/NotificationService.java
@@ -21,29 +21,9 @@ public class NotificationService {
 
     private final TelegramStub telegramStub;
 
-    private void notify(Task task, NotificationType type) {
+    public void notify(Task task, NotificationType type) {
         Notification notification = notificationFactory.createNotification(task, type);
         notificationRepository.save(notification);
-    }
-
-    public void notifyDeveloperMrReviewRequestMessage(Task task) {
-        notify(task, NotificationType.SEND_DEVELOPER_NEW_MR_REQUEST_MESSAGE);
-    }
-
-    public void notifyReviewerNewMrMessage(Task task) {
-        notify(task, NotificationType.SEND_REVIEWER_NEW_MR_MESSAGE);
-    }
-
-    public void notifyDeveloperMergedMrMessage(Task task) {
-        notify(task, NotificationType.SEND_DEVELOPER_MERGED_MR_MESSAGE);
-    }
-
-    public void notifyReviewerThresholdOnMrRequestMessage(Task task) {
-        notify(task, NotificationType.SEND_REVIEWER_THRESHOLD_REQUEST_MESSAGE);
-    }
-
-    public void notifyDeveloperNewFixOnThresholdRequestMessage(Task task) {
-        notify(task, NotificationType.SEND_DEVELOPER_THRESHOLD_FIX_REQUEST_MESSAGE);
     }
 
     public void confirmThresholdTaskNotifyToDeveloper(Task task) {
