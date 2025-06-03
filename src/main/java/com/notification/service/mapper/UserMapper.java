715329f96f9269704744eb8720f8291fbe93baf8
rof1an <egorrachkov0@gmail.com>
2025-06-03 21:20:51 +0300

    added possibility to point out telegram_chat_id while account creating

--- src/main/java/com/notification/service/mapper/UserMapper.java
+++ src/main/java/com/notification/service/mapper/UserMapper.java
@@ -3,10 +3,12 @@ package com.notification.service.mapper;
 import com.notification.service.dto.UserDto;
 import com.notification.service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(unmappedTargetPolicy = ReportingPolicy.WARN, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
 
     User toModel(UserDto userDto);
@@ -14,6 +16,4 @@ public interface UserMapper {
     UserDto toDto(User user);
 
     List<UserDto> toDtoList(List<User> users);
-
-    List<UserDto> toModelList(List<UserDto> users);
 }
